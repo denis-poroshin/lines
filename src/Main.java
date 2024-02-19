9cb6e3328233a1de0f380a4a75adb3561b416254
original <git@github.com:denis-poroshin/variables.git>
2024-02-19 19:19:27 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Задание 2");
+        String newFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + newFullName);
+        System.out.println("Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
